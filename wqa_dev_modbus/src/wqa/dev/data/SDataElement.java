4e39965bd1042b85f8818ba43018b1a714e08efc
chejf <21575602@qq.com>
2020-04-15 14:03:01 +0800

    V4.7.3.0 增加SDataElement复制功能

--- wqa_dev_modbus/src/wqa/dev/data/SDataElement.java
+++ wqa_dev_modbus/src/wqa/dev/data/SDataElement.java
@@ -10,9 +10,21 @@ package wqa.dev.data;
  * @author chejf
  */
 public class SDataElement {
+
     public String name = "";
     public float mainData = 0;
     public boolean internal_only;
-    public String unit= "";
-    public String range_info= "";
+    public String unit = "";
+    public String range_info = "";
+
+    public SDataElement() {
+    }
+
+    public SDataElement(SDataElement other) {
+        this.name = other.name;
+        this.mainData = other.mainData;
+        this.internal_only = other.internal_only;
+        this.unit = other.unit;
+        this.range_info = other.range_info;
+    }
 }
