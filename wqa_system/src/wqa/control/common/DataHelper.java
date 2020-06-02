816711ec9ffd7eebc197ce42d3be725d3b71b5bf
chejf <21575602@qq.com>
2020-06-02 19:54:14 +0800

    V4.8.1.0 漏文件

---
+++ wqa_system/src/wqa/control/common/DataHelper.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wqa.control.common;
+
+import java.util.ArrayList;
+import wqa.adapter.factory.CDevDataTable;
+import wqa.system.WQAPlatform;
+
+/**
+ *
+ * @author chejf
+ */
+public class DataHelper {
+    
+    public static String[] GetSupportDataName(int dev_type) {
+        //单位信息
+        CDevDataTable.DevInfo d_infos = CDevDataTable.GetInstance().namemap.get(dev_type);
+        ArrayList<String> list = new ArrayList();
+        if (d_infos != null) {
+            for (CDevDataTable.DataInfo info : d_infos.data_list) {
+                if (info.internal_only) {
+                    if (WQAPlatform.GetInstance().is_internal) {
+                        list.add(info.data_name);
+                    }
+                } else {
+                    list.add(info.data_name);
+                }
+            }
+        }
+        return list.toArray(new String[0]);
+    }
+    
+    public static String[] GetAllData(int dev_type) {
+        CDevDataTable.DevInfo d_infos = CDevDataTable.GetInstance().namemap.get(dev_type);
+        ArrayList<String> list = new ArrayList();
+        if (d_infos != null) {
+            for (CDevDataTable.DataInfo info : d_infos.data_list) {
+                list.add(info.data_name);
+            }
+        }
+        return list.toArray(new String[0]);
+    }
+
+    public static int GetDataToDBIndex(int dev_type, String data_name) {
+        String[] all_datas = GetAllData(dev_type);
+        for (int i = 0; i < all_datas.length; i++) {
+            if (all_datas[i].contentEquals(data_name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
