87eb9e770356da260fa9ac318f5d70f4515a7e65
chejf <21575602@qq.com>
2020-06-18 13:46:54 +0800

    v4.8.0.2 增加DB接口

---
+++ wqa_dev_migp/src/migp/adapter/OSA/MOSAII_X.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package migp.adapter.OSA;
+
+/**
+ *
+ * @author chejf
+ */
+public class MOSAII_X {
+    
+}
